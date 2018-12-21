5846845794cc62253ab56f68e6bed1ae59ae88aa
Dave Kim <david.kim@thalesesecurity.com>
2018-12-21 13:07:17 +0000

    Day 3, part 1 solution

---
+++ 2018/Day03/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var inputPath = "./input.txt"
+
+// An elf's claim
+type Claim struct {
+	coord Coordinates
+	dimen Dimensions
+}
+
+// The upper left hand position of the claim
+type Coordinates struct {
+	X int
+	Y int
+}
+
+// Width x height of the claim
+type Dimensions struct {
+	X int
+	Y int
+}
+
+func part1() {
+	file, err := os.Open(inputPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var claims []Claim
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		s1 := strings.Split(line, "@")
+		s2 := strings.Split(s1[1], ":")
+		coordinates := s2[0]
+		dimensions := s2[1]
+		s3 := strings.Split(coordinates, ",")
+		x, _ := strconv.Atoi(strings.TrimSpace(s3[0]))
+		y, _ := strconv.Atoi(strings.TrimSpace(s3[1]))
+		coor := Coordinates{x, y}
+		s4 := strings.Split(dimensions, "x")
+		x, _ = strconv.Atoi(strings.TrimSpace(s4[0]))
+		y, _ = strconv.Atoi(strings.TrimSpace(s4[1]))
+		dim := Dimensions{x, y}
+		claim := Claim{coor, dim}
+		claims = append(claims, claim)
+	}
+	// Now plot the fabric values
+	total := 0
+	var fabric [1001][1001]int
+	for _, v := range claims {
+		claimX := v.coord.X
+		claimY := v.coord.Y
+		for i := 0; i < v.dimen.Y; i++ {
+			for j := 0; j < v.dimen.X; j++ {
+				fabric[claimX+j][claimY+i]++
+			}
+		}
+	}
+	for _, v := range fabric {
+		for _, row := range v {
+			if row > 1 {
+				total++
+			}
+		}
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	part1()
+}
