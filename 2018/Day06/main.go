c5224d1b380862b671ed31b0671d5a08b7bc634d
Dave Kim <david.kim@thalesesecurity.com>
2019-01-04 15:30:25 +0000

    Day 6 part 1 solution

--- 2018/Day06/main.go
+++ 2018/Day06/main.go
@@ -21,9 +21,25 @@ func distance(x1 int, x2 int, y1 int, y2 int) int {
 }
 
 func main() {
+	// initialisations
+	//
 	var input []vertex
 	nullVertex := vertex{0, 0}
+	x, y := 0, 0
+	const gridDimension int = 350
+	var grid [gridDimension][gridDimension]vertex
+	for i := range grid {
+		for j := range grid[i] {
+			grid[i][j] = nullVertex
+		}
+	}
+	tl := vertex{0, 0}
+	//tr := vertex{0, 0}
+	//bl := vertex{0, 0}
+	br := vertex{0, 0}
 
+	// Read in input coordinates and identify boundary line
+	//
 	file, err := os.Open(inputPath)
 	if err != nil {
 		panic(err)
@@ -31,129 +47,132 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	var line string
-	x, y := 0, 0
-	const gridDimension int = 500
-	var grid [gridDimension * gridDimension]vertex
-	for i := range grid {
-		grid[i] = nullVertex
-	}
-	name := 1
-	tl := vertex{gridDimension - 1, gridDimension - 1}
-	br := vertex{0, 0}
 
+	minX, minY := gridDimension-1, gridDimension-1
+	maxX, maxY := 0, 0
 	for scanner.Scan() {
 		line = scanner.Text()
 		fmt.Sscanf(line, "%d, %d", &x, &y, err)
 		newVertex := vertex{x, y}
-		// find top left
-		if x <= tl.X && y <= tl.Y {
-			tl = newVertex
+		// find min/max X and Y
+		if x < minX {
+			minX = x
 		}
-		// find bottom right
-		if x >= br.X && y >= br.Y {
-			br = newVertex
+		if x > maxX {
+			maxX = x
+		}
+		if y < minY {
+			minY = y
+		}
+		if y > maxY {
+			maxY = y
 		}
 
 		input = append(input, newVertex)
-		name++
+		grid[x][y] = newVertex
 	}
+
+	tl.X = minX
 	for _, v := range input {
-		grid[v.Y*gridDimension+v.X] = v
+		if v.X == tl.X && v.Y <= tl.Y {
+			tl = v
+		}
 	}
 
 	fmt.Printf("%d coordinates\n", len(input))
 	fmt.Println("Top left ", tl)
 	fmt.Println("Bottom right ", br)
 
-	var borders []vertex
-
+	// use corners to identify boundary
+	//
+	var boundary []vertex
 	for _, v := range input {
-		if v.X == tl.X || v.X == br.X {
-			borders = append(borders, v)
+		if v.X == maxX || v.X == minX {
+			boundary = append(boundary, v)
 			continue
 		}
-		if v.Y == tl.Y || v.Y == br.Y {
-			borders = append(borders, v)
+		if v.Y == maxY || v.Y == minY {
+			boundary = append(boundary, v)
 			continue
 		}
 	}
 
 	fmt.Println("Inputs: ", input)
-	fmt.Println("Borders: ", borders)
+	fmt.Println("Boundary: ", boundary)
 
-	for j := range grid {
-		canWrite := true
-		testX := j % gridDimension
-		testY := int(j / gridDimension)
-		for _, v := range input {
-			if v.X == testX && v.Y == testY {
-				canWrite = false
-			}
-		}
-		if canWrite {
-			dCount := 0
-			dFinal := gridDimension * 2
-			vFinal := nullVertex
+	// Add nullVertex to list to be filtered
+	boundary = append(boundary, nullVertex)
+
+	for X := range grid {
+		for Y := range grid[X] {
+			canWrite := true
+			// search through the inputs and see if this is a coordinate
+			// that should be skipped
 			for _, v := range input {
-				d := distance(testX, v.X, testY, v.Y)
-				if d < dFinal {
-					dFinal = d
-					dCount = 0
-					vFinal = v
-				} else if d == dFinal {
-					dCount++
-					vFinal = nullVertex
+				if v.X == X && v.Y == Y {
+					canWrite = false
 				}
 			}
-			grid[j] = vFinal
-		}
-	}
-	if false {
-		for k, v := range grid {
-			if k%gridDimension == 0 {
-				fmt.Print("\n  ", v, "  ")
+			if canWrite {
+				dCount := 0
+				dFinal := gridDimension + 1
+				vFinal := nullVertex
+				d := 0
+				for _, v := range input {
+					d = distance(X, v.X, Y, v.Y)
+					if d < dFinal {
+						// This is our best candidate
+						dFinal = d
+						vFinal = v
+						// reset the count
+						dCount = 0
+					} else if d == dFinal {
+						// This is a duplicate but might not be
+						// the shortest distance
+						dCount++
+						vFinal = nullVertex
+					}
+				}
+				grid[X][Y] = vFinal
 			} else {
-				fmt.Print("  ", v, "  ")
+				// fmt.Printf("Skipping (%d, %d)\n", X, Y)
 			}
 		}
-		fmt.Print("\n")
 	}
 
 	// shrink the board down to the topleft/bottom right
-	// and add up only those
-
+	//
 	domains := make(map[vertex]int)
 
-	// new dimensions of the grid are based on top left and bottom right
-	newGridLength := br.X - tl.X
-	newGridHeight := br.Y - tl.Y
-
-	// starting index is tl.X + tl.Y*gridDimension
-	for yy := 0; yy < newGridHeight; yy++ {
-		for xx := 0; xx < newGridLength; xx++ {
-			// offset the index
-			index := xx + tl.X + (yy+tl.Y)*gridDimension
-			domains[grid[index]]++
+	for yIndex := minY; yIndex <= maxY; yIndex++ {
+		for xIndex := minX; xIndex <= maxX; xIndex++ {
+			domains[grid[xIndex][yIndex]]++
+			//	fmt.Print(grid[xIndex][yIndex])
 		}
+		//	fmt.Print("\n")
 	}
-	fmt.Println(len(grid))
-	borders = append(borders, nullVertex)
-	for _, i := range borders {
-		fmt.Println("deleting ", i)
+
+	// Filter out coordinates not eligible for largest area
+	//
+	for _, i := range boundary {
+		fmt.Println("Remove ", i)
 		delete(domains, i)
 	}
 
-	fmt.Println(domains)
+	fmt.Println("New area calculations: ", domains)
+	fmt.Println("Finding largest area")
 	maxArea := 0
 	for k, v := range domains {
 		if v >= maxArea {
 			maxArea = v
-			fmt.Println(v, k)
+			fmt.Println(k, v)
 		}
 	}
 
+	// error check
+	//
 	for _, v := range input {
-		if grid[v.Y*gridDimension+v.X] != v {
+		if grid[v.X][v.Y] != v {
 			fmt.Println("Bang ", v.Y, ",", v.X, v)
 		}
 	}
