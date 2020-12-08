2d7b2dbae8e8b259cc9f69161c2b98b2db2e6e76
ddkmm <davedotkim@gmail.com>
2020-12-08 23:35:58 +0000

    AOC 2020: Day 8 quicker solution

--- 2020/08/aoc.py
+++ 2020/08/aoc.py
@@ -3,32 +3,35 @@
 import os
 import re
 from collections import defaultdict
+import time
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-FILEPATH = os.path.join(dir_path, 'input.txt')
-TESTPATH = os.path.join(dir_path, 'testinput.txt')
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
+FILEPATH = os.path.join(DIRPATH, 'input.txt')
+TESTPATH = os.path.join(DIRPATH, 'testinput.txt')
 
-def machine(op, val, int_ptr, acc):
-    if re.search('nop', op):
+def machine(opcode, val, int_ptr, acc, runlog):
+    if runlog[int_ptr] != 0:
+        assert False
+    runlog[int_ptr] += 1
+    if re.search('nop', opcode):
         int_ptr += 1
-    elif re.search('acc', op):
+    elif re.search('acc', opcode):
         acc += val
         int_ptr += 1
-    elif re.search('jmp', op):
+    elif re.search('jmp', opcode):
         int_ptr += val
-    return op, val, int_ptr, acc
+    return opcode, val, int_ptr, acc
 
-def run1(program):
+def run(program):
     int_ptr = 0
     runlog = defaultdict(lambda: 0)
     acc = 0
     while runlog[int_ptr] != 1:
-        runlog[int_ptr] += 1
         inst = program[int_ptr]
-        op = re.split(' ', inst)[0]
+        opcode = re.split(' ', inst)[0]
         val = int(re.split(' ', inst)[1])
-        op, val, int_ptr, acc = machine(op, val, int_ptr, acc)
-    print ("Part 1: {}".format(acc))
+        opcode, val, int_ptr, acc = machine(opcode, val, int_ptr, acc, runlog)
+    print("Part 1: {}".format(acc))
 
 def run_modded(program):
     int_ptr = 0
@@ -37,32 +40,24 @@ def run_modded(program):
     stop = len(program)
     while runlog[int_ptr] != 1:
         if int_ptr == stop:
-            print ("Part 2: {}".format(acc))
+            print("Part 2: {}".format(acc))
             break
-        assert(runlog[int_ptr] != 1)
-        runlog[int_ptr] += 1
         inst = program[int_ptr]
-        op = re.split(' ', inst)[0]
+        opcode = re.split(' ', inst)[0]
         val = int(re.split(' ', inst)[1])
-        op, val, int_ptr, acc = machine(op, val, int_ptr, acc)
-    assert(False)
+        opcode, val, int_ptr, acc = machine(opcode, val, int_ptr, acc, runlog)
+    assert False
 
 def run2(program):
     x = range(0, len(program))
     for i in x:
-        cmd =  program[i]
-        if re.search('nop', cmd):
+        cmd = program[i]
+        if re.search('nop|jmp', cmd):
             back_up = cmd
-            cmd = str(cmd).replace('nop', 'jmp')
-            program[i] = cmd
-            try:
-                run_modded(program)
-            except AssertionError:
-                program[i] = back_up
-            continue
-        elif re.search('jmp', cmd):
-            back_up = cmd
-            cmd = str(cmd).replace('jmp', 'nop')
+            if re.search('nop', cmd):
+                cmd = str(cmd).replace('nop', 'jmp')
+            else:
+                cmd = str(cmd).replace('jmp', 'nop')
             program[i] = cmd
             try:
                 run_modded(program)
@@ -70,13 +65,59 @@ def run2(program):
                 program[i] = back_up
             continue
 
+def run3(program):
+    int_ptr = 0
+    stack = []
+    acc_stack = []
+    runlog = defaultdict(lambda: 0)
+    acc = 0
+    rewind = False
+    while int_ptr < len(program):
+        inst = program[int_ptr]
+        opcode = re.split(' ', inst)[0]
+        val = int(re.split(' ', inst)[1])
+
+        if re.search('nop|jmp', opcode):
+            if not stack:
+                if rewind:
+                    rewind = False
+                else:
+                    if opcode in 'nop':
+                        opcode = str(opcode).replace('nop', 'jmp')
+                    else:
+                        opcode = str(opcode).replace('jmp', 'nop')
+                    # save system state
+                    stack.append(int_ptr)
+                    acc_stack.append(acc)
+                    runlog_back = runlog.copy()
+        try:
+            opcode, val, int_ptr, acc = machine(opcode, val, int_ptr, acc, runlog)
+        except AssertionError:
+            rewind = True
+            # restore system state
+            int_ptr = stack.pop()
+            acc = acc_stack.pop()
+            runlog = runlog_back.copy()
+
+    print("Part 2: {}".format(acc))
+
 def main():
-    print("Day {}".format(os.path.split(dir_path)[1]))
+    print("Day {}".format(os.path.split(DIRPATH)[1]))
 
     with open(FILEPATH) as f:
         program = f.read().splitlines()
-    run1(program)
+
+    # Part 1
+    run(program)
+
+    # Part 2
+    time1 = time.perf_counter()
     run2(program)
+    time2 = time.perf_counter()
+    run3(program)
+    time3 = time.perf_counter()
+    print("Brute force: {} seconds".format(time2-time1))
+    print("Stack unwind: {} seconds".format(time3-time2))
 
 
 main()
