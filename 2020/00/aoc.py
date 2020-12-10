6004e2c98d50e2bcf4a668a39d2a7ae572fbedb9
ddkmm <davedotkim@gmail.com>
2020-12-10 22:01:09 +0000

    AOC 2020: Add timing to template

--- 2020/00/aoc.py
+++ 2020/00/aoc.py
@@ -2,16 +2,26 @@
 
 import os
 import re
+import time
 
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
 FILEPATH = os.path.join(DIRPATH, 'input.txt')
 TESTPATH = os.path.join(DIRPATH, 'testinput.txt')
+DEBUG = False
+
+def part1(data, debug):
+    if debug:
+        print(data)
 
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
 
     with open(FILEPATH) as file:
         data = file.readlines()
-    print(data)
+
+    time1 = time.perf_counter()
+    part1(data, DEBUG)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
 
 main()
