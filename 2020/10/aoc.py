28e1885d788cc7d81baa9c0999d6766fc9c7e04b
ddkmm <davedotkim@gmail.com>
2020-12-10 20:47:18 +0000

    AOC 2020: Add timing functions for Day 10

--- 2020/10/aoc.py
+++ 2020/10/aoc.py
@@ -2,11 +2,13 @@
 
 import os
 from collections import defaultdict
+import time
 
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
 FILE = os.path.join(DIRPATH, 'input.txt')
 TEST = os.path.join(DIRPATH, 'testinput.txt')
 TEST2 = os.path.join(DIRPATH, 'testinput2.txt')
+DEBUG = False
 
 # Given a list of deltas, calculate how many different yet
 # unique and equivalent ways each series of 1s can be
@@ -14,18 +16,50 @@ TEST2 = os.path.join(DIRPATH, 'testinput2.txt')
 def permute(deltas):
     length = len(deltas)
     if length == 1:
+        # (1)
         count = 1
     elif length == 2:
+        # (11, 2)
         count = 2
     elif length == 3:
+        # (111, 12, 21)
         count = 4
     elif length == 4:
+        # (1111, 112, 211, 121, 22, 31, 13)
         count = 7
     else:
+        # we shouldn't hit this but if we do, we'll zero out
         count = 0
-
     return count
 
+def part1(data, debug):
+    diffs = defaultdict(lambda: 0)
+    deltas = []
+    for i in range(1, len(data)):
+        diff = data[i] - data[i-1]
+        deltas.append(diff)
+        diffs[diff] += 1
+    if debug:
+        print(data)
+    print("Part 1: {} * {} = {}".format(diffs[1], diffs[3], diffs[1] * diffs[3]))
+    if debug:
+        print(deltas)
+    return deltas
+
+def part2(deltas, debug):
+    testset = []
+    total = 1
+    for step in deltas:
+        if step == 1:
+            testset.append(step)
+        elif testset:
+            temp = permute(testset)
+            total *= temp
+            if debug:
+                print("permute {} = {}.".format(testset, temp))
+                print("New total is {}".format(total))
+            testset.clear()
+    print("Part 2: {}".format(total))
 
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
@@ -39,27 +73,16 @@ def main():
     data.sort()
     data.append(data[-1] + 3) # add the device
 
-    diffs = defaultdict(lambda: 0)
-    deltas = []
-    for i in range(1, len(data)):
-        diff = data[i] - data[i-1]
-        deltas.append(diff)
-        diffs[diff] += 1
-    print(data)
-    print("Part 1: {} * {} = {}".format(diffs[1], diffs[3], diffs[1] * diffs[3]))
-    print(deltas)
-    testset = []
-    total = 1
-    for step in deltas:
-        if step == 1:
-            testset.append(step)
-        elif testset:
-            temp = permute(testset)
-            total *= temp
-            print("permute {} = {}.".format(testset, temp))
-            print("New total is {}".format(total))
-            testset.clear()
+    time1 = time.perf_counter()
+    deltas = part1(data, DEBUG)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
 
-    print(total)
+    print()
+
+    time1 = time.perf_counter()
+    part2(deltas, DEBUG)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
 
 main()
