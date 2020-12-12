a2b4a8e12a9fae685b09c5c391dfc92b8fc94cbc
ddkmm <davedotkim@gmail.com>
2020-12-12 12:22:11 +0000

    AOC 2020: Day 12 pylinted

--- 2020/12/aoc.py
+++ 2020/12/aoc.py
@@ -7,38 +7,38 @@ import time
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
 FILE = os.path.join(DIRPATH, 'input.txt')
 TEST = os.path.join(DIRPATH, 'testinput.txt')
-DEBUG = True
+DEBUG = False
 
-def Manhattan_distance(x,y):
-    if x < 0:
-        x = -1 * x
-    if y < 0:
-        y = -1 * y
+def m_distance(x_coord, y_coord):
+    if x_coord < 0:
+        x_coord = -1 * x_coord
+    if y_coord < 0:
+        y_coord = -1 * y_coord
 
-    return x + y
+    return x_coord + y_coord
 
 COMPASS = ('N', 'E', 'S', 'W')
-    
+
 class Ferry:
     facing = 'E'
     position = {'X': 0, 'Y': 0}
     compass = {'N': 0, 'E': 1, 'S': 2, 'W': 3}
 
-    def Init(self):
+    def init(self):
         self.facing = 'E'
         self.position['X'] = 0
         self.position['Y'] = 0
 
-    def SetFacing(self, new_facing):
+    def set_facing(self, new_facing):
         self.facing = new_facing
-    
-    def DoTurn(self, turn_inst):
-        self.SetFacing(self.Turn(turn_inst))
+
+    def do_turn(self, turn_inst):
+        self.set_facing(self.turn(turn_inst))
         if DEBUG:
             print("New facing: {}".format(self.facing))
-        
-    def Turn(self, turn_inst):
-        instruction = re.split('(\d+)', turn_inst)
+
+    def turn(self, turn_inst):
+        instruction = re.split(r'(\d+)', turn_inst)
         turns = int(int(instruction[1]) / 90)
 
         # get current heading as a starting point
@@ -50,48 +50,46 @@ class Ferry:
         turns = turns % 4
         return COMPASS[turns]
 
-    def Test_Turn(self):
+    def test_turn(self):
         self.facing = 'E'
-        assert(self.Turn('R90') == 'S')
+        assert self.turn('R90') == 'S'
 
         self.facing = 'E'
-        assert(self.Turn('L90') == 'N')
+        assert self.turn('L90') == 'N'
 
         self.facing = 'E'
-        assert(self.Turn('L180') == 'W')
+        assert self.turn('L180') == 'W'
 
         self.facing = 'E'
-        assert(self.Turn('R180') == 'W')
+        assert self.turn('R180') == 'W'
 
         self.facing = 'W'
-        assert(self.Turn('L180') == 'E')
+        assert self.turn('L180') == 'E'
 
         self.facing = 'W'
-        assert(self.Turn('R180') == 'E')
+        assert self.turn('R180') == 'E'
 
         self.facing = 'E'
-        assert(self.Turn('R270') == 'N')
+        assert self.turn('R270') == 'N'
 
         self.facing = 'E'
-        assert(self.Turn('L270') == 'S')
+        assert self.turn('L270') == 'S'
 
         self.facing = 'N'
-        assert(self.Turn('R90') == 'E')
+        assert self.turn('R90') == 'E'
 
         self.facing = 'N'
-        assert(self.Turn('L90') == 'W')
+        assert self.turn('L90') == 'W'
 
-    def Test(self):
-        self.Test_Turn()
+    def test(self):
+        self.test_turn()
 
-    def Move(self, move_inst):
-        print("Plot course for {}".format(move_inst))
-        inst = re.split('(\d+)', move_inst)
+    def move(self, move_inst):
+        inst = re.split(r'(\d+)', move_inst)
         if inst[0] == 'F':
             heading = self.facing
         else:
             heading = inst[0]
-        print("Current position: {}, {} and heading {}".format(self.position['X'], self.position['Y'], heading))
 
         if heading == 'N':
             self.position['Y'] += int(inst[1])
@@ -102,25 +100,23 @@ class Ferry:
         elif heading == 'E':
             self.position['X'] += int(inst[1])
 
-        print("New position: {}, {}".format(self.position['X'], self.position['Y']))
-
-    def MoveToWaypoint(self, move_inst, waypoint):
-        print("Plot course {} to waypoint".format(move_inst))
-        inst = re.split('(\d+)', move_inst)
+    def move_to_waypoint(self, move_inst, waypoint):
+        inst = re.split(r'(\d+)', move_inst)
         assert inst[0] == 'F'
         multiplier = int(inst[1])
         for temp in waypoint.offset:
             cmd = "{}{}".format(temp,waypoint.offset[temp])
             for _ in range(0,multiplier):
-                self.Move(cmd)
+                self.move(cmd)
 
 
 class Waypoint:
     offset = {'N': 1, 'E': 10, 'S': 0, 'W': 0}
-    
-    def Translate(self, inst):
-        print("Translate waypoint {}".format(inst))
-        instruction = re.split('(\d+)', inst)
+
+    def translate(self, inst):
+        if DEBUG:
+            print("Translate waypoint {}".format(inst))
+        instruction = re.split(r'(\d+)', inst)
         turns = int(int(instruction[1]) / 90)
         new_offset = self.offset.copy()
 
@@ -129,60 +125,58 @@ class Waypoint:
             turns += 2
         # translate values
         for i in range(0,4):
-            try:
-                new_offset[COMPASS[(turns + i) % 4]] = self.offset[COMPASS[i]]
-            except:
-                print("bang")
-        print("{} becomes {}".format(self.offset, new_offset))
+            new_offset[COMPASS[(turns + i) % 4]] = self.offset[COMPASS[i]]
+        if DEBUG:
+            print("{} becomes {}".format(self.offset, new_offset))
         self.offset = new_offset
 
         return 0
-    
-    def Move(self, move_inst):
-        print("Move waypoint {}".format(move_inst))
-        inst = re.split('(\d+)', move_inst)
+
+    def move(self, move_inst):
+        if DEBUG:
+            print("Move waypoint {}".format(move_inst))
+        inst = re.split(r'(\d+)', move_inst)
         assert inst[0] != 'F'
         heading = inst[0]
 
         self.offset[heading] += int(inst[1])
-        print("New waypoint position: {}".format(self.offset))
+        if DEBUG:
+            print("New waypoint position: {}".format(self.offset))
 
         return 0
 
 def part1(nav_data):
     ferry = Ferry()
-    ferry.Init()
+    ferry.init()
     if DEBUG:
-        ferry.Test()
-        ferry.Init()
+        ferry.test()
+        ferry.init()
 
     for inst in nav_data:
-        print(inst)
         if re.search('^R|L',inst):
-            ferry.DoTurn(inst)
+            ferry.do_turn(inst)
         else:
-            ferry.Move(inst)
-    print("Distance travelled: {}".format(Manhattan_distance(ferry.position['X'], ferry.position['Y'])))
+            ferry.move(inst)
+    print("Distance travelled: {}".format(m_distance(ferry.position['X'], ferry.position['Y'])))
 
 
 def part2(nav_data):
     ferry = Ferry()
     waypoint = Waypoint()
-    ferry.Init()
+    ferry.init()
     if DEBUG:
-        ferry.Test()
-        ferry.Init()
+        ferry.test()
+        ferry.init()
 
     for inst in nav_data:
-        print(inst)
         if re.search('^R|L',inst):
-            waypoint.Translate(inst)
+            waypoint.translate(inst)
         elif re.search('^N|E|S|W',inst):
-            waypoint.Move(inst)
+            waypoint.move(inst)
         else:
-            ferry.MoveToWaypoint(inst, waypoint)
+            ferry.move_to_waypoint(inst, waypoint)
 
-    print("Distance travelled: {}".format(Manhattan_distance(ferry.position['X'], ferry.position['Y'])))
+    print("Distance travelled: {}".format(m_distance(ferry.position['X'], ferry.position['Y'])))
 
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
