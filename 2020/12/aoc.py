d75acd9e1ef83ce3f12fc69bafd03fc14dcfdd5c
ddkmm <davedotkim@gmail.com>
2020-12-12 12:07:37 +0000

    AOC 2020: Day 12 finished before lunch!

--- 2020/12/aoc.py
+++ 2020/12/aoc.py
@@ -18,7 +18,7 @@ def Manhattan_distance(x,y):
     return x + y
 
 COMPASS = ('N', 'E', 'S', 'W')
-
+    
 class Ferry:
     facing = 'E'
     position = {'X': 0, 'Y': 0}
@@ -43,15 +43,11 @@ class Ferry:
 
         # get current heading as a starting point
         current = self.compass[self.facing]
-        
-        # and add our new turns in the right direction
-        if instruction[0] == 'R':
-            turns = current + turns
-            turns = turns % 4
-        else:
-            turns = -1 * turns
-            turns = current + turns
-
+        if (turns != 2 and instruction[0] == 'L'):
+            # Add 180 if we need to turn Left
+            turns += 2
+        turns = current + turns
+        turns = turns % 4
         return COMPASS[turns]
 
     def Test_Turn(self):
@@ -66,6 +62,12 @@ class Ferry:
 
         self.facing = 'E'
         assert(self.Turn('R180') == 'W')
+
+        self.facing = 'W'
+        assert(self.Turn('L180') == 'E')
+
+        self.facing = 'W'
+        assert(self.Turn('R180') == 'E')
 
         self.facing = 'E'
         assert(self.Turn('R270') == 'N')
@@ -102,6 +104,50 @@ class Ferry:
 
         print("New position: {}, {}".format(self.position['X'], self.position['Y']))
 
+    def MoveToWaypoint(self, move_inst, waypoint):
+        print("Plot course {} to waypoint".format(move_inst))
+        inst = re.split('(\d+)', move_inst)
+        assert inst[0] == 'F'
+        multiplier = int(inst[1])
+        for temp in waypoint.offset:
+            cmd = "{}{}".format(temp,waypoint.offset[temp])
+            for _ in range(0,multiplier):
+                self.Move(cmd)
+
+
+class Waypoint:
+    offset = {'N': 1, 'E': 10, 'S': 0, 'W': 0}
+    
+    def Translate(self, inst):
+        print("Translate waypoint {}".format(inst))
+        instruction = re.split('(\d+)', inst)
+        turns = int(int(instruction[1]) / 90)
+        new_offset = self.offset.copy()
+
+        if (turns != 2 and instruction[0] == 'L'):
+            # Add 180 if we need to turn Left
+            turns += 2
+        # translate values
+        for i in range(0,4):
+            try:
+                new_offset[COMPASS[(turns + i) % 4]] = self.offset[COMPASS[i]]
+            except:
+                print("bang")
+        print("{} becomes {}".format(self.offset, new_offset))
+        self.offset = new_offset
+
+        return 0
+    
+    def Move(self, move_inst):
+        print("Move waypoint {}".format(move_inst))
+        inst = re.split('(\d+)', move_inst)
+        assert inst[0] != 'F'
+        heading = inst[0]
+
+        self.offset[heading] += int(inst[1])
+        print("New waypoint position: {}".format(self.offset))
+
+        return 0
 
 def part1(nav_data):
     ferry = Ferry()
@@ -118,14 +164,41 @@ def part1(nav_data):
             ferry.Move(inst)
     print("Distance travelled: {}".format(Manhattan_distance(ferry.position['X'], ferry.position['Y'])))
 
+
+def part2(nav_data):
+    ferry = Ferry()
+    waypoint = Waypoint()
+    ferry.Init()
+    if DEBUG:
+        ferry.Test()
+        ferry.Init()
+
+    for inst in nav_data:
+        print(inst)
+        if re.search('^R|L',inst):
+            waypoint.Translate(inst)
+        elif re.search('^N|E|S|W',inst):
+            waypoint.Move(inst)
+        else:
+            ferry.MoveToWaypoint(inst, waypoint)
+
+    print("Distance travelled: {}".format(Manhattan_distance(ferry.position['X'], ferry.position['Y'])))
+
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
 
     with open(FILE) as file:
         data = file.read().splitlines()
 
+    print("Part 1")
     time1 = time.perf_counter()
     part1(data)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
+
+    print("Part 2")
+    time1 = time.perf_counter()
+    part2(data)
     time2 = time.perf_counter()
     print("{} seconds".format(time2-time1))
 
