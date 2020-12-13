20013ab19c9aa45614cf0bf19a03708f6abd2709
ddkmm <davedotkim@gmail.com>
2020-12-13 09:42:21 +0000

    AOC 2020: Day 13, part 2 WIP.

--- 2020/13/aoc.py
+++ 2020/13/aoc.py
@@ -3,6 +3,7 @@
 import os
 import re
 import time
+from collections import defaultdict
 
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
 DATA = os.path.join(DIRPATH, 'input.txt')
@@ -15,8 +16,33 @@ def get_next_bus(bus_id, timestamp):
         current_bus_time += bus_id
     wait_time = current_bus_time - timestamp
     print("wait {} * bus {} = {}".format(wait_time, bus_id, bus_id * wait_time))
-    
-    
+
+def get_bus_sequence(timetable):
+    e_timetable = enumerate(timetable)
+    bus_time = 0
+    res = False
+    while not res:
+        buses = defaultdict(lambda: 0)
+        for offset, bus in e_timetable:
+            if bus != 'x':
+                buses[offset] = bus_time*int(bus) + offset
+        dict_items = buses.items()
+        sorted_items = sorted(dict_items)
+        for offset, bus in sorted_items:
+            if offset == 0:
+                try:
+                    previous = int(bus)
+                except:
+                    print(bus)
+            else:
+                delta = int(bus) - previous
+                if delta != offset:
+                    print(delta, offset)
+                    break
+                else:
+                    print(bus)
+                    continue
+
 def part1(data):
     print("Part 1")
     timestamp = int(data[0])
@@ -27,6 +53,7 @@ def part1(data):
 def part2(data):
     print("Part 2")
     timetable = re.split(r',', data[1])
+    get_bus_sequence(timetable)
 
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
@@ -36,6 +63,11 @@ def main():
 
     time1 = time.perf_counter()
     part1(data)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
+
+    time1 = time.perf_counter()
+    part2(data)
     time2 = time.perf_counter()
     print("{} seconds".format(time2-time1))
 
