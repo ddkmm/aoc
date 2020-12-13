21ba22e6554576f1747413ebc0e4bf770459746f
ddkmm <davedotkim@gmail.com>
2020-12-13 13:19:41 +0000

    AOC 2020: Day 13, part 2 - adding memory

--- 2020/13/aoc.py
+++ 2020/13/aoc.py
@@ -17,40 +17,70 @@ def get_next_bus(bus_id, timestamp):
     wait_time = current_bus_time - timestamp
     print("wait {} * bus {} = {}".format(wait_time, bus_id, bus_id * wait_time))
 
+BUS_OFFSET = 0
+BUS_PERIOD = 1
+BUS_MEMORY = 2
+BUS_MATCH = 3
+
 def get_bus_sequence(timetable):
-    e_timetable = enumerate(timetable)
+    data = []
     final_bus = len(timetable) - 1
+    e_timetable = enumerate(timetable)
+    # make a list of bus data
+    # each bus data contains:
+    #   offset
+    #   period
+    #   memory
+    #   match
+    for offset, bus in e_timetable:
+        if bus != 'x':
+            bus_data = []
+            # offset
+            bus_data.append(offset)
+            # period
+            bus_data.append(int(bus))
+            # memory
+            bus_data.append(int(bus) - offset)
+            # match
+            bus_data.append(False)
+            data.append(bus_data)
 
     # Start counting
     current_tick = 1
     finished = False
     while not finished:
-        e_timetable = enumerate(timetable)
-        times = defaultdict(lambda: 0)
         target = 0
 
-        for offset, bus in e_timetable:
+        for bus_data in data:
             keep_going = False
-            if offset == 0:
-                target = current_tick * int(bus)
-                times[offset] = target
+            nth_bus = 0
+            if bus_data[BUS_OFFSET] == 0:
+                target = current_tick * bus_data[BUS_PERIOD]
                 keep_going = True
-            elif bus != 'x':
-                for i in range(1,current_tick):
-                    current_val = i * int(bus)
-                    current_target = target + offset
-                    if current_val < current_target:
-                        continue
-                    elif i * int(bus) == current_target:
-                        times[offset] = current_target
-                        keep_going = True
-                        if offset == final_bus:
-                            finished == True
+            else:
+                nth_bus += 1
+                current_val = bus_data[BUS_MEMORY]
+                while True:
+                    if current_val > target:
+                        bus_data[BUS_MATCH] = False
+                        keep_going = False
+                        break
+                    elif current_val < target:
+                        bus_data[BUS_MEMORY] = current_val
+                        current_val += bus_data[BUS_PERIOD]
                     else:
+                        assert current_val == target
+                        bus_data[BUS_MEMORY] = current_val
+                        bus_data[BUS_MATCH] = True
+                        if nth_bus == final_bus:
+                            finished = True
+                        keep_going = True
                         break
             if keep_going == False:
                 break
         current_tick += 1
+        if finished:
+            break
 
 def part1(data):
     print("Part 1")
