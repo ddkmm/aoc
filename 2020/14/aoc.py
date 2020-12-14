c834207ffdf74cf8a07601aab6b6ff2163ec5b63
ddkmm <davedotkim@gmail.com>
2020-12-14 21:02:00 +0000

    AOC 2020: Day 14, tidy up

--- 2020/14/aoc.py
+++ 2020/14/aoc.py
@@ -9,28 +9,18 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__))
 DATA = os.path.join(DIRPATH, 'input.txt')
 TEST = os.path.join(DIRPATH, 'testinput.txt')
 TEST2 = os.path.join(DIRPATH, 'testinput2.txt')
-DEBUG = True
-
-def calculate_memory_size(data):
-    max_memory = 0
-    for inst in data:
-        if re.search(r'mem', inst):
-            cmd = re.split(r'=', inst)
-            index = int(re.split(r'(\d+)', str(cmd[0]))[1])
-            if index > max_memory:
-                max_memory = index
-    return max_memory * 2
+DEBUG = False
 
 def make_mask(mask):
     or_mask = ''
     and_mask = ''
-    for b in mask:
-        if b == 'X':
+    for bit in mask:
+        if bit == 'X':
             or_mask += '0'
             and_mask += '1'
         else:
-            or_mask += b
-            and_mask += b
+            or_mask += bit
+            and_mask += bit
     return or_mask, and_mask
 
 def apply_mask(value, or_mask, and_mask):
@@ -38,102 +28,95 @@ def apply_mask(value, or_mask, and_mask):
         print("Before write: {}".format(value))
         print("or_mask {}".format(bin(int(or_mask, 2))))
         print("and_mask {}".format(bin(int(and_mask, 2))))
-    value = value | (int(or_mask, 2))
+    value = value | int(or_mask, 2)
     value = value & int(and_mask, 2)
     if DEBUG:
         print("After write: {}".format(value))
     return value
 
 def replace_x(x_bits, prog):
-    for a in x_bits:
+    for bit in x_bits:
         prog = re.split('X', prog, 1)
-        prog = prog[0] + a + prog[1]
-    print("{}".format(int(prog, 2)))
+        prog = prog[0] + bit + prog[1]
     return prog
 
-def apply_mask2(value, mask, memory, index):
-    value_string = "{0:036b}".format(index)
+def apply_mask2(value, mask, memory, addr):
+    addr_string = "{0:036b}".format(addr)
     if DEBUG:
-        print("{}".format(value_string))
+        print("{}".format(addr_string))
         print("{}".format(mask))
-    value_list = list(value_string)
+    addr_list = list(addr_string)
     x_bit_size = 0
-    for i, b in enumerate(mask):
-        if b == '1':
-            value_list[i] = b
-        if b == 'X':
-            value_list[i] = b
-            x_bit_size += 1 
-    value_string = ""
-    value_string = value_string.join(value_list)
+
+    # process mask
+    for i, bit in enumerate(mask):
+        if bit == '1':
+            addr_list[i] = bit
+        elif bit == 'X':
+            addr_list[i] = bit
+            x_bit_size += 1
+    addr_string = ""
+    addr_string = addr_string.join(addr_list)
 
     # Translate X bits into number of permutations
     float_list = []
-    for n in range(0,2**x_bit_size):
-        n_string = "{0:0b}".format(n)
-        n_string = n_string.zfill(x_bit_size)
-        float_list.append(list(n_string))
-    print(float_list)
+    for bit in range(0,2**x_bit_size):
+        bit_string = "{0:0b}".format(bit)
+        bit_string = bit_string.zfill(x_bit_size)
+        float_list.append(list(bit_string))
+    # apply permutations of X to original address
     for bitmask in float_list:
-        new_index = int(replace_x(bitmask, value_string),2)
-        memory[new_index] = value
-
-
-
-    if DEBUG:
-        print("{0:036b}".format(value))
+        new_addr = int(replace_x(bitmask, addr_string),2)
+        memory[new_addr] = value
     return value
 
 def part1(data):
-    # initialise
-    max_memory = calculate_memory_size(data)
-    memory = [0] * max_memory
-
-    # Run the docking program
-    for inst in data:
-        if re.search(r'mask', inst):
-            mask = re.split(r'=', inst)[1].strip()
-            or_mask, and_mask = make_mask(mask)
-        elif re.search(r'mem', inst):
-            if DEBUG:
-                print("{}".format(inst.strip()))
-            cmd = re.split(r'=', inst)
-            index = int(re.split(r'(\d+)', str(cmd[0]))[1])
-            value = int(cmd[1].strip())
-            # apply mask
-            if DEBUG:
-                print("Before write: {}".format(memory[index]))
-            memory[index] = apply_mask(value, or_mask, and_mask)
-    total = 0
-
-    # Calculate puzzle answer
-    for a in memory:
-        total += a
-    print("Part 1: {}".format(total))
-
-def part2(data):
-    # initialise
     memory = defaultdict(lambda: 0)
 
     # Run the docking program
     for inst in data:
         if re.search(r'mask', inst):
             mask = re.split(r'=', inst)[1].strip()
-        elif re.search(r'mem', inst):
+            or_mask, and_mask = make_mask(mask)
+        else:
             if DEBUG:
                 print("{}".format(inst.strip()))
             cmd = re.split(r'=', inst)
-            index = int(re.split(r'(\d+)', str(cmd[0]))[1])
+            addr = int(re.split(r'(\d+)', str(cmd[0]))[1])
             value = int(cmd[1].strip())
             # apply mask
             if DEBUG:
-                print("Before write: {}".format(memory[index]))
-            apply_mask2(value, mask, memory, index)
+                print("Before write: {}".format(memory[addr]))
+            memory[addr] = apply_mask(value, or_mask, and_mask)
     total = 0
 
     # Calculate puzzle answer
-    for a in memory:
-        total += memory[a]
+    for addr in memory:
+        total += memory[addr]
+    print("Part 1: {}".format(total))
+
+def part2(data):
+    memory = defaultdict(lambda: 0)
+
+    # Run the docking program
+    for inst in data:
+        if re.search(r'mask', inst):
+            mask = re.split(r'=', inst)[1].strip()
+        else:
+            if DEBUG:
+                print("{}".format(inst.strip()))
+            cmd = re.split(r'=', inst)
+            addr = int(re.split(r'(\d+)', str(cmd[0]))[1])
+            value = int(cmd[1].strip())
+            # apply mask
+            if DEBUG:
+                print("Before write: {}".format(memory[addr]))
+            apply_mask2(value, mask, memory, addr)
+    total = 0
+
+    # Calculate puzzle answer
+    for addr in memory:
+        total += memory[addr]
     print("Part 2: {}".format(total))
 
 def main():
@@ -143,7 +126,7 @@ def main():
         data = file.read().splitlines()
 
     time1 = time.perf_counter()
-#    part1(data)
+    part1(data)
     time2 = time.perf_counter()
     print("{} seconds".format(time2-time1))
 
