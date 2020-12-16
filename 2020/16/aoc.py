d69d9c0186b351fd3371927a354bf155563f10d7
Dave Kim <davedotkim@gmail.com>
2020-12-16 13:27:15 +0000

    AOC2020-16: wip

--- 2020/16/aoc.py
+++ 2020/16/aoc.py
@@ -9,15 +9,25 @@ DATA = os.path.join(DIRPATH, 'input.txt')
 TEST = os.path.join(DIRPATH, 'testinput.txt')
 DEBUG = True
 
+def make_rule(rule):
+    assert(len(rule) == 2)
+    rule1 = rule[0]
+    rule2 = rule[1]
+    rule_string = "[{}]|[{}]".format(rule1, rule2)
+    return rule_string
+
+def check_rule(number, rule):
+    return(re.search(rule, number))
+
 def part1(data):
     rule_list = []
     data_start = 0
     for i, line in enumerate(data):
         temp = re.split(r':', line.strip())
         if len(temp) == 2:
-            if re.search(r'your', temp[0]):
+            if re.search(r'^your', temp[0]):
                 continue
-            elif re.search(r'nearby', temp[0]):
+            elif re.search(r'^nearby', temp[0]):
                 data_start = i+1
             else:
                 # this is a rule
@@ -29,15 +39,8 @@ def part1(data):
         for number in re.split(r',',data[line]):
             res = False
             for rule in rule_list:
-                for a in rule:
-                    re_string = ("[{}]".format(a))
-                    if re.search(re_string, number):
-                        res = True
-            if not res:
-                print(number)
-
-                    
-
+                if not check_rule(number, make_rule(rule)):
+                    print(number)
 
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
