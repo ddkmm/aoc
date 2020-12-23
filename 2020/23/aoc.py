fce1258d1a0c4cf260682ca59927bf717c919e74
Dave Kim <davedotkim@gmail.com>
2020-12-23 21:55:11 +0000

    AOC2020-23: Part 2 and clean up

--- 2020/23/aoc.py
+++ 2020/23/aoc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import re
 import time
 
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
@@ -16,9 +15,9 @@ def increment_pointer(cups, pointer):
 
 def get_largest(cups):
     largest = 0
-    for c in cups:
-        if c > largest:
-            largest = c
+    for cup in cups:
+        if cup > largest:
+            largest = cup
     return largest
 
 def part1(cups):
@@ -57,7 +56,7 @@ def part1(cups):
         if DEBUG:
             print("destination: {}".format(dest_cup))
 
-        dest_pointer = increment_pointer(cups, dest_pointer) 
+        dest_pointer = increment_pointer(cups, dest_pointer)
         cups.insert(dest_pointer, removed.pop())
         cups.insert(dest_pointer, removed.pop())
         cups.insert(dest_pointer, removed.pop())
@@ -71,15 +70,154 @@ def part1(cups):
     while cups[print_pointer] != 1:
         output.append(str(cups[print_pointer]))
         print_pointer = increment_pointer(cups, print_pointer)
-    print("".join(output))
+    answer = "".join(output)
+    print("Part 1: {}".format(answer))
+
+class Cup:
+    def __init__(self, label):
+        self.label = label
+        self.next = None
+
+# A better solution using a linked list. Makes part 2 possible.
+def part1_opt(data):
+    cups = {}
+    max_label = 9
+
+    head = data[0]
+    for cup in data:
+        new_cup = Cup(cup)
+        cups[cup] = new_cup
+
+    for i, cup in enumerate(data[:-1]):
+        next_cup = cups[data[i+1]]
+        cups[cup].next = next_cup
+
+    cups[data[-1]].next = cups[head]
+
+    # get starting cup
+    current_cup = cups[data[0]]
+    for i in range(100):
+        if DEBUG:
+            print("-- move {} --".format(i+1))
+            labels = ""
+            cup = current_cup
+            while cup.next.label != current_cup.label:
+                labels = labels + " {}".format(cup.label)
+                cup = cup.next
+            labels = labels + " {}".format(cup.label)
+            print("{}".format(labels))
+            print("current cup {}".format(current_cup.label))
+        removed = [current_cup.next.label, current_cup.next.next.label,
+                   current_cup.next.next.next.label]
+        if DEBUG:
+            print("pick up: [{}, {}. {}]".format(removed[0], removed[1], removed[2]))
+        # update the old loop
+        current_cup.next = current_cup.next.next.next.next
+
+        # get destination cup
+        dest_cup = current_cup.label - 1
+        if dest_cup == 0:
+            dest_cup = max_label
+        while dest_cup in removed:
+            dest_cup -= 1
+            if dest_cup == 0:
+                dest_cup = max_label
+        if DEBUG:
+            print("destination: {}".format(dest_cup))
+
+        # move cups
+        cups[removed[2]].next = cups[dest_cup].next
+        cups[dest_cup].next = cups[removed[0]]
+        # increment index to next current cup
+        current_cup = current_cup.next
+
+    output = []
+    nxt = cups[1].next
+    while nxt.label != 1:
+        output.append(str(nxt.label))
+        nxt = cups[nxt.label].next
+    answer = "".join(output)
+    print("Part 1: {}".format(answer))
+
+def part2(data, total_cups):
+    cups = {}
+    max_label = 1000000
+    rounds = 10000000
+
+    extra_cups = []
+    for i in range(10,total_cups+1):
+        extra_cups.append(i)
+    data = data + extra_cups
+
+    head = data[0]
+    for cup in data:
+        new_cup = Cup(cup)
+        cups[cup] = new_cup
+
+    for i, cup in enumerate(data[:-1]):
+        next_cup = cups[data[i+1]]
+        cups[cup].next = next_cup
+
+    cups[data[-1]].next = cups[head]
+
+    # get starting cup
+    current_cup = cups[data[0]]
+    for i in range(rounds):
+        if DEBUG:
+            print("-- move {} --".format(i+1))
+            labels = ""
+            cup = current_cup
+            while cup.next.label != current_cup.label:
+                labels = labels + " {}".format(cup.label)
+                cup = cup.next
+            labels = labels + " {}".format(cup.label)
+            print("{}".format(labels))
+            print("current cup {}".format(current_cup.label))
+        removed = [current_cup.next.label, current_cup.next.next.label,
+                   current_cup.next.next.next.label]
+        if DEBUG:
+            print("pick up: [{}, {}. {}]".format(removed[0], removed[1], removed[2]))
+        # update the old loop
+        current_cup.next = current_cup.next.next.next.next
+
+        # get destination cup
+        dest_cup = current_cup.label - 1
+        if dest_cup == 0:
+            dest_cup = max_label
+        while dest_cup in removed:
+            dest_cup -= 1
+            if dest_cup == 0:
+                dest_cup = max_label
+        if DEBUG:
+            print("destination: {}".format(dest_cup))
+
+        # move cups
+        cups[removed[2]].next = cups[dest_cup].next
+        cups[dest_cup].next = cups[removed[0]]
+        # increment index to next current cup
+        current_cup = current_cup.next
+
+    print("Part 2: {}".format(cups[1].next.next.label * cups[1].next.next.next.label))
 
 def main():
     print("Day {}".format(os.path.split(DIRPATH)[1]))
 
-    cups = [int(i) for i in list(DATA)]
+    cups = [int(i) for i in list(TEST)]
 
     time1 = time.perf_counter()
     part1(cups)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
+
+    cups = [int(i) for i in list(TEST)]
+    time1 = time.perf_counter()
+    part1_opt(cups)
+    time2 = time.perf_counter()
+    print("{} seconds".format(time2-time1))
+
+    cups = [int(i) for i in list(DATA)]
+    time1 = time.perf_counter()
+    part2(cups, 1000000)
     time2 = time.perf_counter()
     print("{} seconds".format(time2-time1))
 
