43ed2d773112bdf2ad7da53b0a23169061eed16b
Dave Kim <davedotkim@gmail.com>
2021-12-26 21:03:06 +0000

    AOC2021: Day 12, part2

--- 2021/days/12/aoc.go
+++ 2021/days/12/aoc.go
@@ -9,18 +9,16 @@ import (
 )
 
 type Graph struct {
-	Edges   [][]int
-	Labels  map[string]int
-	IDs     map[int]string
-	Visited map[int]bool
+	Edges  [][]int
+	Labels map[string]int
+	IDs    map[int]string
 }
 
 func newGraph(n int) *Graph {
 	return &Graph{
-		Edges:   make([][]int, n),
-		Labels:  make(map[string]int),
-		IDs:     make(map[int]string),
-		Visited: make(map[int]bool),
+		Edges:  make([][]int, n),
+		Labels: make(map[string]int),
+		IDs:    make(map[int]string),
 	}
 }
 
@@ -45,41 +43,6 @@ func (g *Graph) addEdge(u, v int) {
 	g.Edges[v] = append(g.Edges[v], u)
 }
 
-func (g *Graph) getNext(n int) []int {
-	return g.Edges[n]
-}
-
-func (g *Graph) visited(n int, visited map[int]bool) bool {
-	if strings.ToUpper(g.IDs[n]) != g.IDs[n] {
-		return visited[n]
-	} else {
-		return false
-	}
-}
-
-func (g *Graph) findEnd(visited map[int]bool, history []int) [][]int {
-	n := history[len(history)-1]
-	visited[n] = true
-	if g.IDs[n] == "end" {
-		fmt.Printf("Found end: ")
-		history = append(history, n)
-		return [][]int{history}
-	}
-	fmt.Printf("%d edges from node %d: ", len(g.Edges[n]), n)
-	fmt.Println(g.Edges[n])
-	paths := [][]int{}
-	for _, node := range g.getNext(n) {
-		if g.visited(node, visited) {
-			continue
-		}
-		fmt.Printf("Calling findEnd on node %d\n", node)
-		history = append(history, node)
-		newPath := append([]int{}, history...)
-		paths = append(paths, g.findEnd(visited, newPath)...)
-	}
-	return paths
-}
-
 func (g *Graph) findPaths(n int, path []int) [][]int {
 	if g.IDs[n] == "end" {
 		path = append(path, n)
@@ -102,6 +65,34 @@ func (g *Graph) findPaths(n int, path []int) [][]int {
 	return paths
 }
 
+func (g *Graph) findPaths2(n int, path []int, revisit bool) [][]int {
+	if g.IDs[n] == "end" {
+		path = append(path, n)
+		return [][]int{path}
+	}
+	// Check if we've been to lower case nodes before
+	if strings.ToUpper(g.IDs[n]) != g.IDs[n] {
+		for _, c := range path {
+			if n == c {
+				if g.Labels["start"] == n {
+					return [][]int{}
+				} else if revisit {
+					return [][]int{}
+				} else {
+					revisit = true
+				}
+			}
+		}
+	}
+	paths := [][]int{}
+	path = append(path, n)
+	for _, c := range g.Edges[n] {
+		newPath := append([]int{}, path...)
+		paths = append(paths, g.findPaths2(c, newPath, revisit)...)
+	}
+	return paths
+}
+
 func makeCave(text []string) *Graph {
 	n := len(text)
 	cave := newGraph(n)
@@ -116,16 +107,18 @@ func makeCave(text []string) *Graph {
 			}
 		}
 	}
-	fmt.Println(cave.Labels)
+	//	fmt.Println(cave.Labels)
 	for _, line := range text {
 		nodes := strings.Split(line, "-")
-		fmt.Printf("Adding %s to cave system\n", nodes)
+		//		fmt.Printf("Adding %s to cave system\n", nodes)
 		cave.addEdge(cave.Labels[nodes[0]], cave.Labels[nodes[1]])
 	}
 
-	fmt.Println(cave.Edges)
-	cave.printNodes()
-	cave.printEdges()
+	/*
+		fmt.Println(cave.Edges)
+		cave.printNodes()
+		cave.printEdges()
+	*/
 	return cave
 }
 
@@ -133,12 +126,20 @@ func part1(cave *Graph) {
 	defer utils.TimeTrack(time.Now(), "Part 1")
 	var history []int
 	paths := cave.findPaths(cave.Labels["start"], history)
-	fmt.Println(paths)
-	fmt.Printf("%d", len(paths))
+	fmt.Printf("%d\n", len(paths))
+}
+
+func part2(cave *Graph) {
+	defer utils.TimeTrack(time.Now(), "Part 2")
+	var history []int
+	paths := cave.findPaths2(cave.Labels["start"], history, false)
+	fmt.Printf("%d\n", len(paths))
 }
 
 func main() {
-	text := utils.ReadInput(0)
+	text := utils.ReadInput(1)
 	cave := makeCave(text)
 	part1(cave)
+	cave2 := makeCave(text)
+	part2(cave2)
 }
